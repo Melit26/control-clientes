e1ac4aba2157ceacafb9282d5744571dff2b51ff
Melit26 <yuligonzalez26@gmail.com>
2022-08-03 12:16:25 +0200

    cabecero-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { CabeceroComponent } from './componentes/cabecero/cabecero.component';
 import { TableroComponent } from './componentes/tablero/tablero.component';
 import { ClientesComponent } from './componentes/clientes/clientes.component';
-import { EditarClientesComponent } from './componentes/editar-clientes/editar-clientes.component';
+import { EditarClienteComponent } from './componentes/editar-cliente/editar-cliente.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { RegistroComponent } from './componentes/registro/registro.component';
 import { ConfiguracionComponent } from './componentes/configuracion/configuracion.component';
@@ -19,12 +19,13 @@ import { PagePieComponent } from './componentes/page-pie/page-pie.component';
     CabeceroComponent,
     TableroComponent,
     ClientesComponent,
-    EditarClientesComponent,
+    EditarClienteComponent,
     LoginComponent,
     RegistroComponent,
     ConfiguracionComponent,
     NotFoundComponent,
-    PagePieComponent
+    PagePieComponent,
+    EditarClienteComponent
   ],
   imports: [
     BrowserModule,
