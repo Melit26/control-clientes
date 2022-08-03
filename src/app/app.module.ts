241f5aefb9e19ba62e13d77aa4bae3c04c84755e
Melit26 <yuligonzalez26@gmail.com>
2022-08-03 10:57:21 +0200

    Creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceroComponent } from './componentes/cabecero/cabecero.component';
+import { TableroComponent } from './componentes/tablero/tablero.component';
+import { ClientesComponent } from './componentes/clientes/clientes.component';
+import { EditarClientesComponent } from './componentes/editar-clientes/editar-clientes.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { ConfiguracionComponent } from './componentes/configuracion/configuracion.component';
+import { NotFoundComponent } from './componentes/not-found/not-found.component';
+import { PagePieComponent } from './componentes/page-pie/page-pie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceroComponent,
+    TableroComponent,
+    ClientesComponent,
+    EditarClientesComponent,
+    LoginComponent,
+    RegistroComponent,
+    ConfiguracionComponent,
+    NotFoundComponent,
+    PagePieComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componentes/page-pie/page-pie.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-pie',
+  templateUrl: './page-pie.component.html',
+  styleUrls: ['./page-pie.component.css']
+})
+export class PagePieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
